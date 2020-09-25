345ff722799fe718a5301b01b28f9ee87b043e32
Luiz Muller <contact@luizm.dev>
2020-09-25 11:11:52 -0300

    fix typo

--- pkg/network_test.go
+++ pkg/network_test.go
@@ -3,10 +3,10 @@ package whois
 import "testing"
 
 func TestIsIP(t *testing.T) {
-	validaIP := "10.1.1.1"
+	validIP := "10.1.1.1"
 	invalidIP := "10.1.1.256"
-	if !IsIP(validaIP) {
-		t.Errorf("the valid ip was considered invalid %s", validaIP)
+	if !IsIP(validIP) {
+		t.Errorf("the valid ip was considered invalid %s", validIP)
 	}
 
 	if IsIP(invalidIP) {
